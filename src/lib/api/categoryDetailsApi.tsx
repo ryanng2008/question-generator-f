42d5d8cdcdd2f6326a71590b5b4d8102bfd29f66
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-07-28 10:35:12 +0800

    testing replace the root

--- src/lib/api/categoryDetailsApi.tsx
+++ src/lib/api/categoryDetailsApi.tsx
@@ -1,6 +1,6 @@
 export async function fetchCategoryDetails(categoryId: string) {
     try {
-        const response = await fetch(`/api/categorydetails/${categoryId}`);
+        const response = await fetch(`https://66a5a7305dc27a3c190bd6f7.mockapi.io/categorydetails/${categoryId}`);
         if(!response.ok) throw new Error(`Failed to fetch  details for category ID ${categoryId}`)
         const data = await response.json();
         return data;
