f166c3b347afa28e5cf05dfe67c9a5ef351d53bf
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-07-28 10:48:36 +0800

    adding it to categorylist file

--- src/lib/api/apiConfig.ts
+++ src/lib/api/apiConfig.ts
@@ -1,3 +1,7 @@
-const BASE_URL = 'http://qg-env.eba-rskeuxuj.ap-southeast-1.elasticbeanstalk.com/'
+const BASE_URL = 'http://qg-env.eba-rskeuxuj.ap-southeast-1.elasticbeanstalk.com'
+
+//const MOCK_BASE_URL = 'https://66a5a7305dc27a3c190bd6f7.mockapi.io/'
+
+//const DEV_BASE_URL = 'http://127.0.0.1:5000/'
 
 export default BASE_URL
--- src/lib/api/categoryListApi.tsx
+++ src/lib/api/categoryListApi.tsx
@@ -1,6 +1,6 @@
 export async function fetchCategoryList() {
     try {
-        const response = await fetch('api/categorylist');
+        const response = await fetch('https://66a5a7305dc27a3c190bd6f7.mockapi.io/categorylist');
         if(!response.ok) throw new Error('Failed to fetch category list')
         const data = await response.json();
         return data;
