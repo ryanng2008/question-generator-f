e7e9668cf9f52a53d307a302ae67e7245cadec6c
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-07-28 10:42:01 +0800

    actually replace it cuz it didnt save

---
+++ src/lib/api/apiConfig.ts
@@ -0,0 +1,1 @@
+const BASE_URL = 'http://qg-env.eba-rskeuxuj.ap-southeast-1.elasticbeanstalk.com/'
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// PRODUCTION
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,3 +20,19 @@ export default defineConfig({
   }, 
 })
 
+// DEVELOPMENT
+
+//export default defineConfig({
+//  plugins: [react()],
+//  server: {
+//    proxy: {
+//      '/api': {
+//        target: 'https://66a5a7305dc27a3c190bd6f7.mockapi.io/', // http://127.0.0.1:5000/
+//        //http://qg-env.eba-rskeuxuj.ap-southeast-1.elasticbeanstalk.com/
+//        changeOrigin: true,
+//        secure: false,
+//        rewrite: (path) => path.replace(/^\/api/, ''),
+//      },
+//    },
+//  }, 
+//})
