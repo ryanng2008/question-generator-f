cd0d1cf7eed772b468f980a26385111ee1d74043
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2025-03-01 00:20:08 +0800

    mobile 3

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom'
 import Menu from '../assets/svgs/Menu.svg'
 import { useAuth } from '../AuthContext';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Navbar() {
   const { user } = useAuth();
@@ -11,23 +11,23 @@ function Navbar() {
   // }
 
   const NavbarButton = ({ text }: {text: string}) => {
-    return <div className={`rounded-full bg-lightgray hover:underline text-[#444341] py-2 md:px-12 px-5 mx-auto font-semibold tracking-wide text-center`}>
+    return <div className={`rounded-full bg-lightgray hover:underline text-[#444341] py-2 md:px-12 px-4 mx-auto font-semibold tracking-wide md:text-md text-sm text-center`}>
       {text}
       </div>
   }
   return (
     <>
     <div className='bg-[#444341] py-4 w-full'>
-      <div className=" lg:px-12 md:px-8 px-0 mx-4 md:grid grid-cols-3 flex justify-between gap-4">
+      <div className=" lg:px-12 md:px-8 px-0 mx-4 md:grid grid-cols-3 flex justify-between md:gap-4 gap-2">
         <Link to='/'>
           <div className="flex items-center">
-            <img className="h-16" src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3945274.png" alt=""/>
+            <img className="md:h-12 h-10" src="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3945274.png" alt=""/>
             {/* <img className="h-16"src="https://b.fssta.com/uploads/application/soccer/headshots/40670.vresize.350.350.medium.91.png" alt="" /> */}
           </div>
         </Link>
 
         {/* <div className=" items-center md:flex hidden"> */}
-          <ul className='flex justify-between space-x-4 items-center'>
+          <ul className='flex justify-between space-x-6 items-center'>
             <li><Link to='/library'><NavbarButton text='library'/></Link></li>
             <li><Link to='/create'><NavbarButton text='create'/></Link></li>
           </ul>
@@ -36,12 +36,12 @@ function Navbar() {
             {
               (user) 
               ?
-              <img className='max-w-[48px] hover:scale-105 duration-300 fill-mywhite' src={Menu} alt="Menu" /> 
+              <img className='w-12 md:transform-none transform scale-75 md:scale-100 md:font-normal md:hover:scale-105 hover:scale-[85%] duration-300 fill-mywhite' src={Menu} alt="Menu" /> 
               :
               (user === '') 
               ?
-              // <div className='bg-lightgray rounded-full py-2 px-6 hover:scale-105 duration-300 text-darkgray font-semibold'>Log in</div>
-              <AccountCircleIcon sx={{color: "#CBD0D2", fontSize: 50 }} className='hover:scale-105 duration-300'/>
+              <div className='text-white rounded-full py-2 px-6 hover:scale-105 md:text-md text-sm duration-300 font-semibold hover:underline'>Log in</div>
+              // <AccountCircleIcon sx={{color: "#CBD0D2", fontSize: 50 }} className=' hover:scale-105 duration-300'/>
               :
               <div />
             }
